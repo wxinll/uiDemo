6e1cd9fc7f0526f0257a25e8c1aaf87fc9dec4e4
songwx <songwuxin@live.cn>
2018-10-02 16:57:47 +0800

    添加button-group

---
+++ Button-group.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="g-button-group">
+		<slot></slot>
+	</div>
+</template>
+<script>
+	export default {
+		mounted () {
+			console.log(this.$el.children)
+			for (let node of this.$el.children){
+				let name = node.tagName.toLowerCase()
+				if(name !== 'button') {
+					return console.warn(`g-button-group中混入${name}`)
+				}
+			}
+		}
+	}
+</script>
+<style lang="scss" scoped>
+	.g-button-group{
+		display: inline-flex;
+		vertical-align: middle;
+		>.g-button{
+			&:not(:first-child){
+				margin-left: -1px;
+			}
+			&:first-child{
+				border-top-right-radius: 0;
+				border-bottom-right-radius: 0;			
+			}
+			&:last-child{
+				border-top-left-radius: 0;
+				border-bottom-left-radius: 0;			
+			}
+			&:not(:first-child):not(:last-child){
+				border-radius: 0;
+			}
+			&:hover{
+				z-index: 1;
+			}
+		}
+	}
+</style>
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import Button from './Button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './Button-group.vue'
 import Vue from 'vue'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
+Vue.component('g-button-group',ButtonGroup)
 
 new Vue({
 	el: '#app'
